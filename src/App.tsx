9e84bbe8d728d91acc5f45b47bfb56a6c4e085d7
Nathan Rothenberger <nroth10@gmail.com>
2023-07-05 20:14:06 -0500

    little cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { ChakraProvider, Box, Stack, Button } from "@chakra-ui/react";
+import { ChakraProvider, Box, Stack, Button, Text } from "@chakra-ui/react";
 import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 
 const iconLeft = <ArrowBackIcon />;
@@ -59,12 +56,12 @@ const ButtonSizeCollection = (
 };
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ChakraProvider>
-        <div>hi</div>
+        <Box textAlign="center" pb="10">
+          <Text>Component Plane</Text>
+        </Box>
         {/* Buttons */}
         <Box>
           <Stack direction="column" spacing="8">
@@ -75,26 +72,6 @@ function App() {
           </Stack>
         </Box>
       </ChakraProvider>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
