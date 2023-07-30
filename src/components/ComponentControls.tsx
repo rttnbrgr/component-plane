a87297e7c402fa41aece5b12c2f3783a8d4a2d38
Nathan Rothenberger <nroth10@gmail.com>
2023-07-29 19:28:56 -0500

    wip: component group control

--- src/components/ComponentControls.tsx
+++ src/components/ComponentControls.tsx
@@ -22,6 +22,45 @@ export const ComponentControls = ({
   visibleComponentGroups,
   setVisibleComponentGroups,
 }: ComponentControlProps) => {
+  function handleGroupToggle(x, y: boolean) {
+    console.log("---");
+    console.log("handle group toggle for ", x);
+    console.log("val:", y);
+
+    setVisibleComponents((prev: componentStateObjectType) => {
+      // Copy object
+      const duplicate = { ...prev };
+      // Walk componets + set
+      x.components.forEach(component => {
+        // Update
+        duplicate[component] = y;
+        // duplicate[cmp] = !check2;
+      });
+      // Return
+      return duplicate;
+    });
+
+    setVisibleComponentGroups(prev => {
+      // Copy object
+      const duplicate = { ...prev };
+
+      // const check = duplicate[x.id];
+      // console.log("duplicate", duplicate);
+      // console.log("x", x);
+      // console.log("x.id", x.id);
+
+      // console.log("duplicate[form]", duplicate["form"]);
+      // console.log("duplicate[x.id]", duplicate[x.id]);
+
+      // console.log("check", check);
+
+      // Look up group; Update val
+      duplicate[x.id] = y;
+      // Return
+      return duplicate;
+    });
+  }
+
   return (
     <Box>
       <Stack direction={"row"} spacing={"6"} width="100%" flexWrap={"wrap"}>
@@ -34,20 +73,28 @@ export const ComponentControls = ({
             }}
             key={i}
           >
-            <ComponentControlSwitch id={`${i}-${group.id}`} isHeader>
+            <ComponentControlSwitch
+              id={`${i}-${group.id}`}
+              isHeader
+              isChecked={visibleComponentGroups[group.id]}
+              onChange={e => {
+                handleGroupToggle(group, e.target.checked);
+              }}
+            >
               {group.id}
             </ComponentControlSwitch>
 
+            {/* Components Rendered */}
             <SimpleGrid minChildWidth="150px" spacing="2" spacingX={"6"} mt="2">
               {group.components.map((cmp, j) => {
-                const check = visibleComponents["Button"];
+                // const check = visibleComponents["Button"];
                 const check2: boolean = visibleComponents[cmp];
-                console.log("cmp: ", cmp);
-                console.log("check: ", check);
-                console.log("check2: ", check2);
+                // console.log("cmp: ", cmp);
+                // console.log("check: ", check);
+                // console.log("check2: ", check2);
                 // const isChecked = visibleComponents[cmp]
                 const handleToggle = () => {
-                  console.log("handle toggle for ", cmp);
+                  // console.log("handle toggle for ", cmp);
                   setVisibleComponents((prev: componentStateObjectType) => {
                     // Copy object
                     const duplicate = { ...prev };
@@ -59,7 +106,7 @@ export const ComponentControls = ({
                 };
                 return (
                   <ComponentControlSwitch
-                    id="foo"
+                    id={`${j}-${cmp}`}
                     key={j}
                     isChecked={check2}
                     onChange={handleToggle}
